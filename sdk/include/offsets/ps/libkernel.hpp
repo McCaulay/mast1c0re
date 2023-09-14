fc3618d90926a662599b666bdbef1f1d3ab6fd11
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2023-09-14 15:54:36 -0600

    Add PS4 FW v11.00 support & PS5 FW v3.00 - 4.51 support

--- sdk/include/offsets/ps/libkernel.hpp
+++ sdk/include/offsets/ps/libkernel.hpp
@@ -15,20 +15,28 @@
         #include <offsets/ps/libkernel/ps4/10.01.hpp>
     #elif defined(FIRMWARE) && FIRMWARE >= 1050 && FIRMWARE <= 1071
         #include <offsets/ps/libkernel/ps4/10.50_10.71.hpp>
+    #elif defined(FIRMWARE) && FIRMWARE == 1100
+        #include <offsets/ps/libkernel/ps4/11.00.hpp>
     #endif
 
     #if defined(LIB_KERNEL_SYS_RET_ERROR) && defined(FIRMWARE) && FIRMWARE < 1001
         #define SYSCALL_SUPPORT true
     #endif
 #elif defined(PS5) && PS5
-    #if defined(FIRMWARE) && FIRMWARE == 403
+    #if defined(FIRMWARE) && FIRMWARE >= 300 && FIRMWARE <= 321
+        #define LIB_KERNEL_SYS_RET_ERROR 0x35b6
+    #elif defined(FIRMWARE) && FIRMWARE >= 400 && FIRMWARE <= 451
         #define LIB_KERNEL_SYS_RET_ERROR 0x35d6
     #elif defined(FIRMWARE) && FIRMWARE >= 650 && FIRMWARE <= 761
         #define LIB_KERNEL_SYS_RET_ERROR 0x35f6
     #endif
 
-    #if defined(FIRMWARE) && FIRMWARE == 403
-        #include <offsets/ps/libkernel/ps5/4.03.hpp>
+    #if defined(FIRMWARE) && FIRMWARE >= 300 && FIRMWARE <= 321
+        #include <offsets/ps/libkernel/ps5/3.00_4.21.hpp>
+    #elif defined(FIRMWARE) && FIRMWARE >= 400 && FIRMWARE <= 403
+        #include <offsets/ps/libkernel/ps5/4.00_4.03.hpp>
+    #elif defined(FIRMWARE) && FIRMWARE >= 450 && FIRMWARE <= 451
+        #include <offsets/ps/libkernel/ps5/4.50_4.51.hpp>
     #elif defined(FIRMWARE) && FIRMWARE == 650
         #include <offsets/ps/libkernel/ps5/6.50.hpp>
     #endif
